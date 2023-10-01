8a8e2e05835e7e2aa15644b9a558e353a0145da5
Huy0110 <duchuy01102001@gmail.com>
2023-10-01 10:57:55 +0000

    Add create game messages

--- x/checkers/types/full_game.go
+++ x/checkers/types/full_game.go
@@ -20,11 +20,11 @@ func (storedGame StoredGame) GetRedAddress() (red sdk.AccAddress, err error) {
 func (storedGame StoredGame) ParseGame() (game *rules.Game, err error) {
 	board, errBoard := rules.Parse(storedGame.Board)
 	if errBoard != nil {
-			return nil, sdkerrors.Wrapf(errBoard, ErrGameNotParseable.Error())
+		return nil, sdkerrors.Wrapf(errBoard, ErrGameNotParseable.Error())
 	}
 	board.Turn = rules.StringPieces[storedGame.Turn].Player
 	if board.Turn.Color == "" {
-			return nil, sdkerrors.Wrapf(fmt.Errorf("turn: %s", storedGame.Turn), ErrGameNotParseable.Error())
+		return nil, sdkerrors.Wrapf(fmt.Errorf("turn: %s", storedGame.Turn), ErrGameNotParseable.Error())
 	}
 	return board, nil
 }
@@ -32,11 +32,11 @@ func (storedGame StoredGame) ParseGame() (game *rules.Game, err error) {
 func (storedGame StoredGame) Validate() (err error) {
 	_, err = storedGame.GetBlackAddress()
 	if err != nil {
-			return err
+		return err
 	}
 	_, err = storedGame.GetRedAddress()
 	if err != nil {
-			return err
+		return err
 	}
 	_, err = storedGame.ParseGame()
 	return err
